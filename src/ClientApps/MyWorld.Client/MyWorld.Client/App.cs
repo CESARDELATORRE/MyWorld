d6164d4497af09e36587e39f3463ba140b201992
Cesar De la Torre <cesardl@microsoft.com>
2016-03-29 16:17:50 -0700

    Initial Skeleton Solution for Xamarin.Forms client apps

---
+++ src/ClientApps/MyWorld.Client/MyWorld.Client/App.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace MyWorld.Client.Core
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            MainPage = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children = {
+                        new Label {
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            //(CDLTLL-Deprecated)XAlign = TextAlignment.Center,
+                            Text = "Welcome to MY WORLD!"
+                        }
+                    }
+                }
+            };
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
